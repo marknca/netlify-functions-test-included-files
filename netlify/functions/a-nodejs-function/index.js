1b235a0cb7de29f154dc0635b0a21e9609cfd9fe
Mark Nunnikhoven <me@markn.ca>
2022-09-07 16:01:02 -0400

    Tracking down bug for included_files functionality

--- netlify/functions/a-nodejs-function/index.js
+++ netlify/functions/a-nodejs-function/index.js
@@ -1,6 +1,16 @@
 exports.handler = async function (event, context) {
+    const fs = require('fs');
+    let fns = [];
+    //const path = require("path");
+    //let currentPath = path.resolve(path.basename(__dirname))
+    //let currentFile = path.join(currentPath, 'index.js')
+    fs.readdir('.', (err, files) => {
+        files.forEach(file => {
+            fns.push(file);
+        });
+    });
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "a-nodejs-function TEST MESSAGE", }),
+        body: JSON.stringify({ message: "a-nodejs-function TEST MESSAGE", files: fns }),
     };
 };
